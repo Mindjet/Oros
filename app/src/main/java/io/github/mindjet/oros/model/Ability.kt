12644e400d9a8779f3e554116aa6b119c15d167f
Mindjet <pearl920@outlook.com>
2018-11-29 23:18:06 +0800

    [+]: add hero ability list

--- app/src/main/java/io/github/mindjet/oros/model/Ability.kt
+++ app/src/main/java/io/github/mindjet/oros/model/Ability.kt
@@ -1,8 +1,16 @@
 package io.github.mindjet.oros.model
 
+import io.github.mindjet.oros.R
+import io.github.mindjet.oros.recyclerview.base.ILayoutId
+
 data class Ability(
         val name: String,
         val description: String,
         val icon: String,
         val video: String
-)
+): ILayoutId {
+    override fun getLayoutId(): Int {
+        return R.layout.item_ability
+    }
+
+}
--- app/src/main/java/io/github/mindjet/oros/recyclerview/HeroDetailAdapter.kt
+++ app/src/main/java/io/github/mindjet/oros/recyclerview/HeroDetailAdapter.kt
@@ -1,14 +1,19 @@
 package io.github.mindjet.oros.recyclerview
 
+import android.support.v7.widget.LinearLayoutManager
 import io.github.mindjet.oros.ext.load
 import io.github.mindjet.oros.ext.log
 import io.github.mindjet.oros.model.Hero
 import io.github.mindjet.oros.recyclerview.base.CommonRecyclerViewAdapter
 import io.github.mindjet.oros.recyclerview.base.CommonViewHolder
+import kotlinx.android.synthetic.main.content_ability.view.*
 import kotlinx.android.synthetic.main.content_basic_info.view.*
 import kotlinx.android.synthetic.main.content_slogan.view.*
 
 class HeroDetailAdapter : CommonRecyclerViewAdapter<Hero>() {
+
+    private lateinit var abilityAdapter: HeroAbilityAdapter
+
     override fun onBindViewHolder(holder: CommonViewHolder, position: Int) {
         val hero = data[position]
         log(hero)
@@ -19,6 +24,11 @@ class HeroDetailAdapter : CommonRecyclerViewAdapter<Hero>() {
             tv_hero_affiliation.text = hero.affiliation
             tv_hero_base.text = hero.base
             tv_hero_bio.text = hero.bio
+            abilityAdapter = HeroAbilityAdapter()
+            recycler_view.adapter = abilityAdapter
+            recycler_view.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            abilityAdapter.data.addAll(hero.abilitySet)
+            abilityAdapter.notifyDataSetChanged()
         }
     }
 
